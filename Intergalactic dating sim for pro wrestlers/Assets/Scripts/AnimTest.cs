4090e43874b484b90863e5b209d7e2be716a9efb
Joshua Stankiewicz <shotgunjesus@gmail.com>
2017-01-21 17:09:33 -0600

    animations and music

---
+++ Intergalactic dating sim for pro wrestlers/Assets/Scripts/AnimTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTest : MonoBehaviour
+{
+
+    public Animation anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+		if (anim == null)
+        {
+            anim = GetComponent<Animation>();
+        }
+        anim.Play();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
