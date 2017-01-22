048d64dbb6ff83969f00d5c97beebd673f7d0d82
Joshua Stankiewicz <shotgunjesus@gmail.com>
2017-01-22 00:26:37 -0600

    Level transitions

--- Intergalactic dating sim for pro wrestlers/Assets/Scripts/MainMenuScript.cs
+++ Intergalactic dating sim for pro wrestlers/Assets/Scripts/MainMenuScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
@@ -21,6 +22,7 @@ public class MainMenuScript : MonoBehaviour
     public void StartGame()
     {
         Debug.Log("Start the game!");
+        SceneManager.LoadScene("RoundOne");
     }
 
     public void QuitGame()
