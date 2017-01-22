5583d7951c918628158d8eeef8cc65c2bfd0cdf5
Joshua Stankiewicz <shotgunjesus@gmail.com>
2017-01-21 19:42:58 -0600

    Extracted animation clips from FBX files. Test prefab with Animation Controller made for The Rock.

--- Intergalactic dating sim for pro wrestlers/Assets/Scripts/AnimTest.cs
+++ Intergalactic dating sim for pro wrestlers/Assets/Scripts/AnimTest.cs
@@ -5,21 +5,60 @@ using UnityEngine;
 public class AnimTest : MonoBehaviour
 {
 
-    public Animation anim;
+    public string animClip = "Idle";
+    Animator anim;
 
 	// Use this for initialization
 	void Start ()
     {
-		if (anim == null)
-        {
-            anim = GetComponent<Animation>();
-        }
-        anim.Play();
+        anim = GetComponent<Animator>();
+
+        anim.Play(animClip);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        // Simple calling convention for playing animations.
+        // They MUST be in the Animator tree in the editor to 
+        // be played properly.
+
+        // TODO: figure out how to make one-off animations auto
+        // transition back to default animation loop (Idle)
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            anim.Play("Bashful");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            anim.Play("Beckon");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            anim.Play("Excited");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            anim.Play("Idle");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            anim.Play("SadIdle");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            anim.Play("SadWalk");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            anim.Play("Walking");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            anim.Play("Waving");
+        }
+
+    }
+
 }
