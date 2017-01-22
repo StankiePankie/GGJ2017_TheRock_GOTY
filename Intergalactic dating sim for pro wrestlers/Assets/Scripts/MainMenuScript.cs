8c44036c361a18b55a290d99b19d35cc302d5480
Joshua Stankiewicz <shotgunjesus@gmail.com>
2017-01-21 23:54:00 -0600

    main menu progress

---
+++ Intergalactic dating sim for pro wrestlers/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void StartGame()
+    {
+        Debug.Log("Start the game!");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game should be closing now.");
+        Application.Quit();
+    }
+}
